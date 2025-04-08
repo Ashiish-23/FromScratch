9df9353769279ad131ff80bf26f819e51b1f1261
Ashish A Lopis <145143261+Ashiish-23@users.noreply.github.com>
2025-04-08 19:51:59 +0530

    Delete Natural Language Processing directory

--- Natural Language Processing/Prog05/Prog05.py
+++
@@ -1,5 +0,0 @@
-from nltk.stem import PorterStemmer
-import nltk; nltk.download('punkt')
-
-ps = PorterStemmer()
-print([ps.stem(w) for w in ["running", "happiness", "playing", "argued", "flies", "denied", "agreeing", "singing"]])
